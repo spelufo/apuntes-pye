ba1040a948cb726597666cf30f544d66bd00e069
Santiago Pelufo <santiagopelufo@gmail.com>
2014-01-21 22:57:27 -0200

    Atrasado con el P4

---
+++ pye.py
@@ -0,0 +1,23 @@
+def factorial(n, down_to=0, ammount=None):
+    n = int(n)
+    down_to = int(down_to)
+    if ammount is not None:
+    	ammount = int(ammount)
+    if n < 2 or ammount == 0 or n == down_to:
+        return 1
+    return n * factorial(n - 1, down_to=down_to, ammount=(ammount-1 if type(ammount) == int else None))
+
+def arreglos(n, k):
+    return factorial(n, ammount=k)
+A = arreglos
+
+def combinaciones(n, k):
+    return factorial(n, ammount=k) / factorial(k)
+binomial = C = combinaciones
+
+def permutaciones(n, reps=[]):
+    res = factorial(n)
+    for i in reps:
+        res /= factorial(i)
+    return res
+P = permutaciones
